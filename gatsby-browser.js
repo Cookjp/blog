fa3f704312dd470f9da3246ccc8cc010faed9340
Cookjp <jamescook214@gmail.com>
2022-05-04 22:52:58 +0100

    fixes broken CSS classes on first render

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,15 @@ import AppBar from 'Components/appbar/AppBar';
 import ThemeProvider, { FancyBorder } from 'Components/layout/ThemeProvider';
 import { DarkModeProvider } from 'Components/layout/DarkModeProvider';
 
+const ReactDOM = require('react-dom');
+
+// https://github.com/gatsbyjs/gatsby/issues/17676
+// https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/#replaceHydrateFunction
+// fixes incorrect css classes on first render
+export const replaceHydrateFunction = () => (element, container, callback) => {
+  ReactDOM.render(element, container, callback);
+};
+
 export const wrapRootElement = ({ element }) => (
   <DarkModeProvider>
     <ThemeProvider>
