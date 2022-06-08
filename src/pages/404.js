79029ef64406cfd64615d12d2de7a4726737a367
Cookjp <jamescook214@gmail.com>
2022-06-08 23:29:22 +0100

    improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,58 +1,33 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
-
-// styles
-const pageStyles = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4,
-};
+import { Button, Typography } from '@mui/material';
+import PageTransition from '../components/layout/PageTransition';
 
 // markup
 const NotFoundPage = () => (
-  <main style={pageStyles}>
-    <title>Not found</title>
-    <h1 style={headingStyles}>Page not found</h1>
-    <p style={paragraphStyles}>
-      Sorry
+  <PageTransition>
+    <main>
+      <title>Not found</title>
+      <h1>Page not found</h1>
+      <Typography variant="h3">Sorry 😔</Typography>
       {' '}
-      <span role="img" aria-label="Pensive emoji">
-        😔
-      </span>
-      {' '}
-      we couldn’t find what you were looking for.
-      <br />
+      <Typography variant="h4">we couldn’t find what you were looking for.</Typography>
       {process.env.NODE_ENV === 'development' ? (
         <>
           <br />
           Try creating a page in
           {' '}
-          <code style={codeStyles}>src/pages/</code>
+          <code>src/pages/</code>
           .
           <br />
         </>
       ) : null}
       <br />
-      <Link to="/">Go home</Link>
-      .
-    </p>
-  </main>
+      <Link to="/"><Button variant="contained" sx={{ mb: 2 }}><Typography variant="p">Go home</Typography></Button></Link>
+      <br />
+      {' '}
+    </main>
+  </PageTransition>
 );
 
 export default NotFoundPage;
